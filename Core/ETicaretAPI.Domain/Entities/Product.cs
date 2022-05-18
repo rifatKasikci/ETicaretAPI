a4aba5cbc6b860abe0b65016797584f485611a29
rifatKasikci <78976774+rifatKasikci@users.noreply.github.com>
2022-05-18 23:00:20 +0300

    Add project files.

---
+++ Core/ETicaretAPI.Domain/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using ETicaretAPI.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string ProductName { get; set; }
+        public int Stock { get; set; }
+        public long Price { get; set; }
+    }
+}
---
+++ Presentation/ETicaretAPI.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var result = _productService.GetProducts();
+            return Ok(result);
+        }
+    }
+}
